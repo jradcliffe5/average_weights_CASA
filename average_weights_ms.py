78fdd3d03deea16ab5f206ba9a78b0a582a88efe
Jack Radcliffe <jack.radcliffe@postgrad.manchester.ac.uk>
2018-01-04 17:28:31 +0100

    v0.01

--- average_weights_ms.py
+++ average_weights_ms.py
@@ -1,3 +1,9 @@
+### CASA script to expunge average weights and weight_spectrum from a CASA measurement set
+## Author: J. Radcliffe, 2018
+# If you use this script, please acknowledge me
+
+import os
+import numpy as np
 t = tbtool()
 ## Inputs ############################################################
 '''
@@ -21,19 +27,48 @@ def pull_baselines(msfile):
                 a = [t.getcell(colname, j)]+[t.getcell('ANTENNA2', j)]
                 if (a not in baseline_distribution) == True:
                     baseline_distribution = baseline_distribution + [a]
-    return baseline_distribution
+    return np.array(baseline_distribution)
 
+def find_baseline_arg(baseline,baseline_distribution):
+    j = 0
+    for i in baseline_distribution:
+        if np.array_equal(i, baseline) == True:
+            index = j
+        j += 1
+    try:
+        return index
+    except UnboundLocalError:
+        print 'Baseline distribution doesn\'nt match, Quitting'
+        exit()
 
-x = pull_baselines(theconcatvis)
-print np.array(x).T
-'''
+baseline_distribution = pull_baselines(theconcatvis)
+weight_table = np.append(baseline_distribution,np.zeros((baseline_distribution.shape[0],baseline_distribution.shape[1]+1)),axis=1)
+
+np.save('baseline_distribution.npy',baseline_distribution)
 t.open(theconcatvis, nomodify=False)
-for colname in [ 'WEIGHT', 'WEIGHT_SPECTRUM']:
+for colname in ['WEIGHT','WEIGHT_SPECTRUM']:
     if (colname in t.colnames()) and (t.iscelldefined(colname,0)):
         for j in xrange(0,t.nrows()):
             a = t.getcell(colname, j)
-            print a
-'''
+            flags = t.getcell('FLAG', j)
+            if colname == 'WEIGHT':
+                flag_weight = [np.all(flags[0]),np.all(flags[1])]
+                a = np.delete(a,np.argwhere(flag_weight)==True) ## Delete flagged entries
+                if a.size != 0: ## Catches the data which is flagged i.e. WEIGHTS==0
+                    baseline = np.array([t.getcell('ANTENNA1',j),t.getcell('ANTENNA2',j)])
+                    baseline_arg = find_baseline_arg(baseline,baseline_distribution)
+                    weight_table[baseline_arg][2] = weight_table[baseline_arg][2] + np.average(a)
+                    weight_table[baseline_arg][4] = weight_table[baseline_arg][4] + 1
+            if colname == 'WEIGHT_SPECTRUM':
+                flag_weight = flags
+                a = np.delete(a,np.argwhere(flag_weight)==True) ## Delete flagged entries
+                if a.size != 0: ## Catches the data which is flagged i.e. WEIGHTS==0
+                    baseline = np.array([t.getcell('ANTENNA1',j),t.getcell('ANTENNA2',j)])
+                    baseline_arg = find_baseline_arg(baseline,baseline_distribution)
+                    weight_table[baseline_arg][3] = weight_table[baseline_arg][3] + np.average(a)
+                    #weight_table[baseline_arg][3] = weight_table[baseline_arg][3] + 1
+np.save('weights.npy',weight_table)
+
 '''
 for colname in ['SIGMA']:
 	if (wscale > 0. and colname in t.colnames()) and (t.iscelldefined(colname,0)):
@@ -43,4 +78,5 @@ for colname in ['SIGMA']:
 			a *= sscale
 			t.putcell(colname, j, a)
 '''
+os.system('rm casa*log')
 t.close()

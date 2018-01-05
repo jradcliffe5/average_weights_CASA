02638d8bce02f6a2eed8c3f93b61f6629b9190e3
Jack Radcliffe <jack.radcliffe@postgrad.manchester.ac.uk>
2018-01-05 15:11:00 +0100

    del

--- weight_convert_to_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os, sys, re
-import numpy as np
-from code_mailer import headless
-
-measurement_set = sys.argv[sys.argv.index('weight_convert_to_csv.py')+1]
-
-column_names = ['ANTENNA1','ANTENNA2','WEIGHT_average','WT_SPECTRUM_average','integrations']
-pd.DataFrame(data=weights, columns=column_names).to_csv('%s_av_wt.csv' % measurement_set.split('.ms')[0],index=False)
